1e5a3296ff8b4a187e21e3e3e777e858bb8518c4
Maurice <59695433+mauricevf@users.noreply.github.com>
2025-03-04 13:41:33 +0100

    Database
    Category
    Lessons
    LessonUser
    Users Table

---
+++ Models/lessonUserModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const lessonUserSchema = new mongoose.Schema({
+    user_id: { type: Number, required: true },
+    lesson_id: { type: Number, required: true },
+});
+
+const LessonUser = mongoose.model('LessonUser', lessonUserSchema);
+
+export default LessonUser;
