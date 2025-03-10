d98b8d06d8f5323d4d00792ff7c9385cc4d7b377
sildatema <sildatema04@gmail.com>
2025-03-10 13:52:16 +0100

    api key frontend connection fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, apikey')
     next()
 })
 
