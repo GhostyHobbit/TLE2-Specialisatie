80cc87d8d933134b37e4d768509cccb8f2f31e82
marijnvdw <m.vdwaterlaat@gmail.com>
2025-03-12 15:20:14 +0100

    Cors fixed i hope

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, apikey')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
     next()
 })
 
