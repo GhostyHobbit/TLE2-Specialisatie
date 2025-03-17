2deca5b8e44338eb4de5461d0916abedfac782dc
sildatema <sildatema04@gmail.com>
2025-03-17 13:13:36 +0100

    changes

--- Models/knnModel.js
+++ Models/knnModel.js
@@ -2,11 +2,9 @@ import { kNear } from 'knear'; // Destructure the correct function
 
 const knn = new kNear(3); // Use the actual function
 
-knn.learn([1, 2], 'A');
-knn.learn([3, 4], 'B');
-knn.learn([5, 6], 'B');
-knn.learn([7, 8], 'B');
-knn.learn([0, 1], 'A');
-knn.learn([2, 3], 'A');
+knn.learn([5, 1], "Hello");
+knn.learn([2, 0], "Peace");
+knn.learn([1, 1], "Thumbs Up");
+knn.learn([3, 0], "OK");
 
 export const classifyData = (input) => knn.classify(input);
